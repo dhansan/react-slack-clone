e548acb4f61b2dfd216690fb0156d141540ad755
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-09-13 13:29:18 +0700

    onAuthStateChanged to Listen for LoginRegister

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
 //Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from 'react-router-dom';
 // Styles
 import 'semantic-ui-css/semantic.min.css';
 // Link
 import App from './components/App';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
+import firebase from './components/Firebase/firebase';
 import * as serviceWorker from './serviceWorker';
 
-const Root = () => {
-  return (
-    <Router>
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.props.history.push('/');
+      }
+    });
+  }
+
+  render() {
+    return (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
       </Switch>
-    </Router>
-  );
-};
+    );
+  }
+}
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const RootwithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootwithAuth />
+  </Router>,
+  document.getElementById('root')
+);
 serviceWorker.unregister();
