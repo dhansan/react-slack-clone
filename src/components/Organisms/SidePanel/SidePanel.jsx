483b42a8a544279a412a01d480d8dadccd63dae3
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-09-14 20:19:45 +0700

    App Header  UserPanel Dropdown

--- src/components/Organisms/SidePanel/SidePanel.jsx
+++ src/components/Organisms/SidePanel/SidePanel.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
+import UserPanel from './UserPanel';
 
 class SidePanel extends Component {
   render() {
@@ -11,7 +12,7 @@ class SidePanel extends Component {
         vertical
         style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
       >
-        <USerPanel />
+        <UserPanel />
       </Menu>
     );
   }
