23af174ab4027821fa35de1c4a94b7f646660a28
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-09-14 20:03:21 +0700

    Scaffold App Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,28 @@
+//Libraries
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+// Styles
 import './App.css';
+// Link
+import ColorPanel from './Organisms/ColorPanel/ColorPanel';
+import Messages from './Organisms/Messages/Messages';
+import MetaPanel from './Organisms/MetaPanel/MetaPanel';
+import SidePanel from './Organisms/SidePanel/SidePanel';
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Grid columns="equal" className="app" style={{ background: '#eee' }}>
+      <ColorPanel />
+      <SidePanel />
+
+      <Grid.Column style={{ marginLeft: 320 }}>
+        <Messages />
+      </Grid.Column>
+
+      <Grid.Column width={4}>
+        <MetaPanel />
+      </Grid.Column>
+    </Grid>
   );
 };
 
