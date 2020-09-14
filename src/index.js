193400e82e736dd8338b6c0b52578e62ba316451
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-09-14 13:31:39 +0700

    Set up Redux to Make User Data Available on Global State

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import {
   Route,
   withRouter,
 } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 // Styles
 import 'semantic-ui-css/semantic.min.css';
 // Link
@@ -14,12 +17,18 @@ import App from './components/App';
 import Register from './components/Auth/Register';
 import Login from './components/Auth/Login';
 import firebase from './components/Firebase/firebase';
+import rootReducer from './components/Redux/reducers';
+import { setUser } from './components/Redux/actions';
 import * as serviceWorker from './serviceWorker';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
+        // console.log(user);
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     });
@@ -36,12 +45,14 @@ class Root extends React.Component {
   }
 }
 
-const RootwithAuth = withRouter(Root);
+const RootwithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-  <Router>
-    <RootwithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootwithAuth />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 serviceWorker.unregister();
