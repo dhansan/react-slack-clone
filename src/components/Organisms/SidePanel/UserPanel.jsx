b84b5c5d8719198f8922a1f59ae26836a6c21697
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-09-15 09:01:48 +0700

    Add Avatar to User Dropdown

--- src/components/Organisms/SidePanel/UserPanel.jsx
+++ src/components/Organisms/SidePanel/UserPanel.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
 import firebase from '../../Config/Firebase/firebase';
 
 class UserPanel extends Component {
-  dropDownOptions = () => [
+  state = {
+    user: this.props.currentUser,
+  };
+
+  dropdownOptions = () => [
     {
       key: 'user',
       text: (
         <span>
-          Signed in as <strong>User</strong>
+          Signed in as <strong>{this.state.user.displayName}</strong>
         </span>
       ),
-      disable: true,
+      disabled: true,
     },
     {
       key: 'avatar',
@@ -27,24 +31,31 @@ class UserPanel extends Component {
     firebase
       .auth()
       .signOut()
-      .then(() => console.log('sign out!'));
+      .then(() => console.log('signed out!'));
   };
 
   render() {
+    const { user } = this.state;
     return (
       <Grid style={{ background: '#4c3c4c' }}>
         <Grid.Column>
           <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
+            {/* App Header */}
             <Header inverted floated="left" as="h2">
               <Icon name="code" />
               <Header.Content>DevChat</Header.Content>
             </Header>
 
-            {/* User Dropdown */}
-            <Header style={{ padding: '0.25rem' }} as="h4" inverted>
+            {/* User Dropdown  */}
+            <Header style={{ padding: '0.25em' }} as="h4" inverted>
               <Dropdown
-                trigger={<span>User</span>}
-                options={this.dropDownOptions()}
+                trigger={
+                  <span>
+                    <Image src={user.photoURL} spaced="right" avatar />
+                    {user.displayName}
+                  </span>
+                }
+                options={this.dropdownOptions()}
               />
             </Header>
           </Grid.Row>
