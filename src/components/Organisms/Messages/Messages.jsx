beaa19dcfbfa543be4be26b9eb6cdadead8afaaf
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-09-18 09:11:39 +0700

    Scaffold ColorPanel  Message Components

--- src/components/Organisms/Messages/Messages.jsx
+++ src/components/Organisms/Messages/Messages.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Segment, Comment } from 'semantic-ui-react';
+
+import MessageForm from './MessageForm';
+import MessagesHeader from './MessagesHeader';
 
 class Messages extends Component {
   render() {
     return (
-      <div>
-        <p>Messages</p>
-      </div>
+      <React.Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        </Segment>
+
+        <MessageForm />
+      </React.Fragment>
     );
   }
 }
