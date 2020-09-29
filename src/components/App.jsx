b95e066353e3891cc351aa6dcc70f3bcd86d39b4
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-09-29 22:22:45 +0700

    Attach Chosen Colors to User Data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,10 @@ import SidePanel from './Organisms/SidePanel/SidePanel';
 
 const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
   <Grid columns="equal" className="app" style={{ background: '#eee' }}>
-    <ColorPanel />
+    <ColorPanel
+      key={currentUser && currentUser.name}
+      currentUser={currentUser}
+    />
     <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
 
     <Grid.Column style={{ marginLeft: 320 }}>
@@ -26,7 +29,7 @@ const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => (
 
     <Grid.Column width={4}>
       <MetaPanel
-        key={currentChannel && currentChannel.id}
+        key={currentChannel && currentChannel.name}
         userPosts={userPosts}
         currentChannel={currentChannel}
         isPrivateChannel={isPrivateChannel}
