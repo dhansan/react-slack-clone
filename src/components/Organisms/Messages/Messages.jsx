06fea1b2d54f8cf77e5d449c4915a0ead021a59c
Dhan Satriyo <dhanu.satriyo@gmail.com>
2020-10-02 08:54:34 +0700

    Add Typing Component  Animation, Display in Messages

--- src/components/Organisms/Messages/Messages.jsx
+++ src/components/Organisms/Messages/Messages.jsx
@@ -7,6 +7,7 @@ import { Segment, Comment } from 'semantic-ui-react';
 import MessageForm from './MessageForm';
 import MessagesHeader from './MessagesHeader';
 import Message from './Message';
+import Typing from './Typing';
 
 class Messages extends Component {
   state = {
@@ -194,6 +195,9 @@ class Messages extends Component {
             {searchTerm
               ? this.displayMessages(searchResults)
               : this.displayMessages(messages)}
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <span className="user__typing">scott is typing</span> <Typing />
+            </div>
           </Comment.Group>
         </Segment>
 
